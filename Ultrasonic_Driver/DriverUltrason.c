601b10f64d516831185af8d8e1e0fc53b67095f3
Asma REBHI <rebhi.asma22@gmail.com>
2025-06-26 12:05:25 +0200

    Ultrasonic driver

---
+++ Ultrasonic_Driver/DriverUltrason.c
@@ -0,0 +1,262 @@
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/platform_device.h>
+#include <linux/of.h>
+#include <linux/miscdevice.h>
+#include <linux/gpio/consumer.h>
+#include <linux/interrupt.h>
+#include <linux/ktime.h>
+#include <linux/delay.h>
+#include <linux/uaccess.h>
+
+static const struct of_device_id ultrasonic_of_match[] = {
+    { .compatible = "comp,ultrasonic" },
+    {},
+};
+MODULE_DEVICE_TABLE(of, ultrasonic_of_match);
+
+struct mydev_misc_t {
+    struct miscdevice miscdev;
+    const char* device_name;
+    struct gpio_desc *gpio_trig;
+    struct gpio_desc *gpio_echo;
+};
+
+static int irq_echo;
+static volatile ktime_t start, end;
+static volatile int measuring_in_progress = 0;
+static volatile unsigned int measured_data = 0;
+
+static DECLARE_WAIT_QUEUE_HEAD(measure_wq);
+
+// Ultrasonic Sensor Measurement
+//use the ECHO pin as an interrupt in an ultrasonic sensor 
+static irqreturn_t echo_irq_handler(int irq, void *dev_id) {
+
+    int gpio_val = gpiod_get_value(((struct mydev_misc_t *)dev_id)->gpio_echo);
+    
+    if (!measuring_in_progress) {
+        pr_info(" pas de mesure en cours\n");
+        return IRQ_HANDLED;
+    }
+   
+    if (gpio_val) {
+        // Front montant : begin of measure 
+        start = ktime_get();
+    } else {
+        
+        end = ktime_get();
+
+        uint64_t diff_time_ns = ktime_to_ns(ktime_sub(end, start));
+        u32 diff_us = (u32)div_u64(diff_time_ns, 1000);  // ns -> us
+        u32 distance_cm = (diff_us * 343) / 20000;       // calcul distance en cm
+
+        measured_data = distance_cm;
+
+        measuring_in_progress = 0;
+        wake_up_interruptible(&measure_wq);
+    }
+    return IRQ_HANDLED;
+}
+
+// Write : envoie au trigger
+// on utilise container_of pour récupérer un pointeur vers notre structure "device"
+
+static ssize_t start_measure(struct mydev_misc_t *dev) {
+    if (measuring_in_progress) {
+        pr_info("Mesure déjà en cours\n");
+        return -EBUSY;
+    }
+
+    measured_data = 0;
+    measuring_in_progress = 1;
+
+    // Déclenchement du trigger
+    gpiod_set_value(dev->gpio_trig, 0);
+    udelay(2);
+    gpiod_set_value(dev->gpio_trig, 1);
+    udelay(10);
+    gpiod_set_value(dev->gpio_trig, 0);
+
+    pr_info("Trigger envoyé, attente de l'echo...\n");
+
+    return 0;
+}
+
+
+static ssize_t my_read(struct file *filp, char __user *buf, size_t count, loff_t *ppos) {
+    struct mydev_misc_t *dev = container_of(filp->private_data, struct mydev_misc_t, miscdev);
+    char kbuf[32];
+    size_t len;
+
+    if (*ppos != 0)
+        return 0;
+
+    int ret = start_measure(dev);
+    if (ret < 0)
+        return ret;
+
+    // Attente de fin de mesure, timeout à 100 ms
+    //measure_wq : c’est la wait queue (file d’attente) sur laquelle le processus va attendre.
+    /*
+    !measuring_in_progress : c’est la condition d’arrêt de l’attente.
+
+    Le processus va dormir tant que cette condition est fausse.
+
+    Ici, il attend que measuring_in_progress soit à 0, donc que la mesure soit finie.
+
+    msecs_to_jiffies(100) : c’est le timeout maximum d’attente, converti en "jiffies" (unités de temps du kernel).
+    
+    */
+    /*
+    Le processus qui appelle wait_event_interruptible_timeout va se mettre en sommeil (ne rien faire) tant que :
+
+    La condition !measuring_in_progress est fausse (c’est-à-dire que measuring_in_progress est toujours vrai, mesure en cours)
+
+    Et que le timeout n’est pas dépassé.
+
+    Si pendant l’attente la condition devient vraie (la mesure est terminée et measuring_in_progress = 0), alors le processus se réveille immédiatement.
+
+    Sinon, il se réveille automatiquement quand le timeout (100 ms ici) est écoulé.
+
+    Si un signal externe interrompt l’attente, la fonction retourne aussi.
+    
+    
+    
+    */
+   /*
+   Si pendant 100 ms l’interruption d’écho ne se produit pas (pas de front descendant),
+
+    la fonction retourne un timeout (-ETIMEDOUT) et le processus my_read() se termine proprement sans rester bloqué
+   
+   */
+    ret = wait_event_interruptible_timeout(measure_wq, !measuring_in_progress, msecs_to_jiffies(100));
+    if (ret == 0) {
+        pr_info("Timeout lors de la lecture de la mesure\n");
+        return -ETIMEDOUT;
+    }
+
+    len = snprintf(kbuf, sizeof(kbuf), "%u\n", measured_data);
+    if (count < len)
+        return -EINVAL;
+
+    if (copy_to_user(buf, kbuf, len))
+        return -EFAULT;
+
+    *ppos += len;
+    return len;
+}
+
+static const struct file_operations my_fops = {
+    .owner = THIS_MODULE,
+    .read = my_read,
+    
+};
+
+static int ultrasonic_probe(struct platform_device *pdev) {
+    int ret;
+    struct mydev_misc_t *mydev;
+
+    // 1. Allocation mémoire
+    mydev = devm_kzalloc(&pdev->dev, sizeof(*mydev), GFP_KERNEL);
+    if (!mydev)
+        return -ENOMEM;
+
+    // 2. Stocker dans les données privées du périphérique
+    platform_set_drvdata(pdev, mydev);
+
+    // 3. Lire l’étiquette (label)
+    ret = of_property_read_string(pdev->dev.of_node, "label", &mydev->device_name);
+    if (ret) {
+        dev_err(&pdev->dev, "Failed to read label property\n");
+        return ret;
+    }
+
+    // 4. Enregistrer le périphérique character
+    mydev->miscdev.minor = MISC_DYNAMIC_MINOR;
+    mydev->miscdev.name = mydev->device_name;
+    mydev->miscdev.fops = &my_fops;
+    mydev->miscdev.mode = 0666;
+
+    // 5. Récupérer les GPIOs
+    mydev->gpio_trig = gpiod_get(&pdev->dev, "trig", GPIOD_OUT_LOW);
+    if (IS_ERR(mydev->gpio_trig)) {
+        dev_err(&pdev->dev, "Failed to get trig GPIO\n");
+        return PTR_ERR(mydev->gpio_trig);
+    }
+
+    mydev->gpio_echo = gpiod_get(&pdev->dev, "echo", GPIOD_IN);
+    if (IS_ERR(mydev->gpio_echo)) {
+        dev_err(&pdev->dev, "Failed to get echo GPIO\n");
+        gpiod_put(mydev->gpio_trig);
+        return PTR_ERR(mydev->gpio_echo);
+    }
+
+    // 6. Configurer les directions
+    gpiod_direction_output(mydev->gpio_trig, 0);
+    gpiod_direction_input(mydev->gpio_echo);
+
+    // 7. Obtenir l’IRQ
+    irq_echo = platform_get_irq(pdev, 0);
+    if (irq_echo <= 0) {
+        dev_err(&pdev->dev, "platform_get_irq failed\n");
+        gpiod_put(mydev->gpio_echo);
+        gpiod_put(mydev->gpio_trig);
+        return irq_echo;
+    }
+
+    pr_info("IRQ echo is: %d\n", irq_echo);
+
+    // 8. Enregistrer le handler IRQ
+    ret = devm_request_irq(&pdev->dev, irq_echo, echo_irq_handler, IRQF_SHARED, "ultrasonic_irq", mydev);
+    if (ret) {
+        dev_err(&pdev->dev, "Failed to request IRQ\n");
+        gpiod_put(mydev->gpio_echo);
+        gpiod_put(mydev->gpio_trig);
+        return ret;
+    }
+
+    // 9. Enregistrement du périphérique
+    ret = misc_register(&mydev->miscdev);
+    if (ret) {
+        dev_err(&pdev->dev, "Failed to register misc device\n");
+        gpiod_put(mydev->gpio_echo);
+        gpiod_put(mydev->gpio_trig);
+        return ret;
+    }
+
+    pr_info("Driver %s initialise avec IRQ\n", mydev->device_name);
+    return 0;
+}
+
+static void ultrasonic_remove(struct platform_device *pdev) {
+    struct mydev_misc_t *misc_dev = platform_get_drvdata(pdev);
+
+    // 1. Désenregistrer le périphérique character
+    misc_deregister(&misc_dev->miscdev);
+    pr_info("Driver %s retired\n", misc_dev->device_name);
+
+    // 2. Libérer les GPIOs
+    if (misc_dev->gpio_trig)
+        gpiod_put(misc_dev->gpio_trig);
+    if (misc_dev->gpio_echo)
+        gpiod_put(misc_dev->gpio_echo);
+
+    pr_info("Ultrasonic GPIO Libered.\n");
+}
+
+static struct platform_driver ultrasonic_driver = {
+    .driver = {
+        .name = "ultrasonic_driver",
+        .of_match_table = ultrasonic_of_match,
+        .owner = THIS_MODULE,
+    },
+    .probe = ultrasonic_probe,
+    .remove = ultrasonic_remove,
+};
+
+module_platform_driver(ultrasonic_driver);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Asma REBHI");
+MODULE_DESCRIPTION("Driver HC-SR04 avec interruption (comme bouton)");
---
+++ Ultrasonic_Driver/README.md
@@ -0,0 +1,80 @@
+#  Linux Kernel Driver for Ultrason 
+This Linux kernel module implements a platform driver and exposes a miscellaneous character device interface for measure a distance via Ultrasonic devices.
+
+# Features
+
+- Platform driver bound via Device Tree using the compatible :  
+  `" compatible = "comp,ultrasonic"; "`
+
+# Hardware Interface
+
+- Requires only an available (non-reserved) 2 GPIOs pins.
+- Ensure the GPIOs used are not already taken by another peripheral.
+
+# Ultrasonic Sensor response
+
+  - The ultrasonic sensor uses a transmitter that emits sound waves at frequencies        inaudible to humans.
+
+  - When an object is present, the emitted sound reflects off the object.
+
+  - The receiver captures the echoed sound within a certain time frame.
+
+  - Based on the time elapsed between transmission and reception, the sensor calculates the distance to the object.
+
+`
+
+# Ultrasonic Sensor Measurement Steps
+
+  * Trigger pulse sent:
+  A 10 µs pulse is sent to the TRIG pin → this starts the measurement.
+
+  * Ultrasonic emission:
+  The sensor emits 8 ultrasonic pulses at 40 kHz (~200 µs duration).
+
+  * Echo waiting:
+  The ECHO pin goes HIGH when the signal is sent.
+
+  * Echo reception:
+  The ECHO pin goes LOW when the echo is received → this marks the end of the measurement.
+
+  * Time calculation:
+  Measure the duration for which ECHO was HIGH → this is the round-trip time.
+
+  * Distance conversion:
+  ```dts
+  Distance=(SpeedofSound × Time)/2
+  ```
+
+  * Wait before next measurement:
+  * Wait at least 60 ms before starting a new measurement for reliable results.
+  
+# Device Tree 
+
+```dts
+	ultrasonic {
+			compatible = "comp,ultrasonic";
+			label = "ultrasonic";
+			pinctrl-names = "default";
+			pinctrl-0 = <&echo_ultrasonic>;
+			trig-gpios = <&gpio 5 GPIO_ACTIVE_HIGH>;   // GPIO5 - TRIG (output)
+			echo-gpios = <&gpio 6 GPIO_ACTIVE_HIGH>;   // GPIO6 - ECHO (input)
+			interrupt-parent = <&gpio> ;
+			interrupts = <6 IRQ_TYPE_EDGE_BOTH>;
+			status = "okay";
+		};
+
+&gpio{
+	echo_ultrasonic: echo_ultrasonic {
+		brcm,pins = <6>;
+		brcm,function = <0>; 
+	};
+} 
+```
+# Usage
+
+- The driver accepts:
+  - `echo 0 > /dev/led` → **Turns ON** the LED  
+  - `echo 1 > /dev/led` → **Turns OFF** the LED
+
+- To read the current state of the LED:
+  - `cat /dev/led`
